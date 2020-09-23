5c515232b85bfd9154219c6e79a2a31e07fd1cea
Akshit Batra <akbatra567@gmail.com>
2020-09-23 22:53:52 +0530

    Updates

---
+++ precache-manifest.ba108bd9e8a4f03a52d503357b41cd31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e4f0ee6d7686d96457ae2768ce6892",
+    "url": "/MyKraftySkills-Organization.github.io/code-pen-clone/index.html"
+  },
+  {
+    "revision": "a591767d9de1c92c14b4",
+    "url": "/MyKraftySkills-Organization.github.io/code-pen-clone/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "fcaeda58fce82b83dbc4",
+    "url": "/MyKraftySkills-Organization.github.io/code-pen-clone/static/css/main.03e06907.chunk.css"
+  },
+  {
+    "revision": "a591767d9de1c92c14b4",
+    "url": "/MyKraftySkills-Organization.github.io/code-pen-clone/static/js/2.05b1cb86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyKraftySkills-Organization.github.io/code-pen-clone/static/js/2.05b1cb86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcaeda58fce82b83dbc4",
+    "url": "/MyKraftySkills-Organization.github.io/code-pen-clone/static/js/main.ab7efe9f.chunk.js"
+  },
+  {
+    "revision": "88614373f3053af733bb",
+    "url": "/MyKraftySkills-Organization.github.io/code-pen-clone/static/js/runtime-main.8eb72460.js"
+  }
+]);
